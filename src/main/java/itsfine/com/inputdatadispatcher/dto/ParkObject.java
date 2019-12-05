6fa20bbf2562fa6c49476ec21f50654e52829d85
bartoshevichm <bartoshevich.m@gmail.com>
2019-12-05 11:12:26 +0200

    ver with getting Array json

--- src/main/java/itsfine/com/inputdatadispatcher/dto/ParkObject.java
+++ src/main/java/itsfine/com/inputdatadispatcher/dto/ParkObject.java
@@ -4,13 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Record {
-    String parking_id;
-    String car_number;
-    LocalDate date_time;
+public class ParkObject {
+    private int parking_id;
+    private String car_number;
+    private LocalDateTime date_time;
 }
--- src/main/java/itsfine/com/inputdatadispatcher/service/InputDataDispatcherService.java
+++ src/main/java/itsfine/com/inputdatadispatcher/service/InputDataDispatcherService.java
@@ -1,37 +1,53 @@
 package itsfine.com.inputdatadispatcher.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import itsfine.com.inputdatadispatcher.dto.Record;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import itsfine.com.inputdatadispatcher.dto.ParkObject;
 import itsfine.com.inputdatadispatcher.interfaces.IInputDataDispatcher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.integration.support.MessageBuilder;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
-@EnableBinding(Processor.class)
+@EnableBinding(IInputDataDispatcher.class)
 public class InputDataDispatcherService {
-    private ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
     //regex for israel car numbers (7 digits with hyphen after second and 5th digit  (1980-2017),
     // 8 digits with hyphen after 3th and 5th digit (2017-now))
-    @Value("${car_number_validation_reg_ex:(\\d{2}-\\d{3}-\\d{2}|\\d{3}-\\d{2}-\\d{3})")
+    @Value("${car_number_validation_reg_ex:\\d{2}-\\d{3}-\\d{2}|\\d{3}-\\d{2}-\\d{3}}")
     private String carNumberValidationRegEx;
-    @Autowired
-    IInputDataDispatcher inputDataDispatcher;
+
+    final IInputDataDispatcher inputDataDispatcher;
+
+    public InputDataDispatcherService(IInputDataDispatcher inputDataDispatcher) {
+        this.inputDataDispatcher = inputDataDispatcher;
+    }
 
     @StreamListener(IInputDataDispatcher.INPUT)
-    public void dispatchInputData(String recordStrData) throws IOException {
-        Record record = mapper.readValue(recordStrData, Record.class);
-        if (Pattern.matches(carNumberValidationRegEx, record.getCar_number())) {
-            inputDataDispatcher.output()
-                    .send(MessageBuilder.withPayload(recordStrData).build());
-        } else {
-            inputDataDispatcher.invalid()
-                    .send(MessageBuilder.withPayload(recordStrData).build());
+    public void dispatchInputData(String parkObjectDataArr) throws IOException {
+        ArrayList<ParkObject> parkObjectsList = getParkObjects(parkObjectDataArr);
+        for (ParkObject parkObj : parkObjectsList) {
+            if (isMatches(parkObj)) {
+                inputDataDispatcher.output()
+                        .send(MessageBuilder.withPayload(parkObj).build());
+            } else {
+                inputDataDispatcher.invalid()
+                        .send(MessageBuilder.withPayload(parkObj).build());
+            }
         }
+    }
+
+    private boolean isMatches(ParkObject parkObj) {
+        return Pattern.matches(carNumberValidationRegEx, parkObj.getCar_number());
+    }
+
+    private ArrayList<ParkObject> getParkObjects(String parkObjectDataArr) throws IOException {
+        CollectionType type = mapper.getTypeFactory()
+                .constructCollectionType(ArrayList.class, ParkObject.class);
+        return mapper.readValue(parkObjectDataArr, type);
     }
 }
